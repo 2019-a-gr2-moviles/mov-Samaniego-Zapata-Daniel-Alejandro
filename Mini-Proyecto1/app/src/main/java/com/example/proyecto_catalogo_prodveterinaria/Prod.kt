cb3558c22c81afcbf1f599286f15ce6904dca90a
Daniel Samaniego <daniel.samaniego4@hotmail.com>
2019-05-16 23:07:47 -0500

    botones

--- Mini-Proyecto1/app/src/main/java/com/example/proyecto_catalogo_prodveterinaria/Prod.kt
+++ Mini-Proyecto1/app/src/main/java/com/example/proyecto_catalogo_prodveterinaria/Prod.kt
@@ -2,24 +2,3 @@ package com.example.proyecto_catalogo_prodveterinaria
 
 import com.google.gson.annotations.SerializedName
 
-data class Producto(
-    @SerializedName("id") val id: Int,
-    @SerializedName("tipoProducto") val tipoProducto: String,
-    @SerializedName("percha") val percha: Int
-)
-
-class Post {
-    var id: Number? = null
-    var tipoProducto: String? = null
-    var percha: Number? = null
-    //var postTag: List<String>? = null
-
-    constructor() : super() {}
-
-    constructor(Id: Number, Tipo_Producto: String, Percha: Number) : super() {
-        this.id = Id
-        this.tipoProducto = Tipo_Producto
-        this.percha = Percha
-    }
-
-}
--- MyPrimeraApplication2/app/src/main/java/com/example/myprimeraapplication/MainActivity.kt
+++ MyPrimeraApplication2/app/src/main/java/com/example/myprimeraapplication/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.example.myprimeraapplication
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_actividad_dos.setOnClickListener {
+            //this.irAActividadDos()
+            irAActividadDos()
+        }
     }
+
+    fun irAActividadDos(){
+        val intent = Intent(this, Actividad_Dos::class.java)
+        startActivity(intent)
+    }
+
+
 }
+
+

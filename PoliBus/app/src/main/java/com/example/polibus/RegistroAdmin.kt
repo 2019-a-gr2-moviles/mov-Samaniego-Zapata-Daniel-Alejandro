cf0cd44ed349f1f617211765134fe47c1750c52a
Daniel Samaniego <daniel.samaniego@epn.edu.ec>
2019-07-10 22:04:03 -0500

    login

--- PoliBus/app/src/main/java/com/example/polibus/RegistroAdmin.kt
+++ PoliBus/app/src/main/java/com/example/polibus/RegistroAdmin.kt
@@ -1,19 +1,12 @@
 package com.example.polibus
 
-import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
 
-class Login : AppCompatActivity() {
+class RegistroAdmin : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
-
-
-
+        setContentView(R.layout.activity_registro_admin)
     }
-
-
 }
